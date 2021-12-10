118d6f19d1aff784bef861b79bc64a262e7cdd93
Jaimin <jaimin.solanki@techholding.co>
2021-12-10 18:56:08 +0530

    feat: add linters

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-const express = require('express');
-const swaggerUi = require('swagger-ui-express');
-const swaggerYaml = require('yamljs');
+const express = require("express");
+const swaggerUi = require("swagger-ui-express");
+const swaggerYaml = require("yamljs");
 
 const app = express();
 
-const swaggerYamlDocument = swaggerYaml.load('./swagger.yaml');
+const swaggerYamlDocument = swaggerYaml.load("./swagger.yaml");
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerYamlDocument));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerYamlDocument));
 
 app.listen(3000, () => {
   // eslint-disable-next-line no-console
-  console.log('server listening on port 8000!');
+  console.log("server listening on port 8000!");
 });
