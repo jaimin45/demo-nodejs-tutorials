514c675b1de54b366b1f658354b5c1a2c7bae4e8
jaimin solanki <solankijaimin40@gmail.com>
2021-12-15 15:25:46 +0530

    feat: create model

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,11 @@
 /* eslint-disable spellcheck/spell-checker */
-import express from 'express';
+import express from "express";
 
 const router = express.Router();
 
 /* GET home page. */
-router.get('/', (_req, res) => {
-  res.render('index', { title: 'Express' });
+router.get("/", (_req, res) => {
+  res.render("index", { title: "Express" });
 });
 
 export default router;
