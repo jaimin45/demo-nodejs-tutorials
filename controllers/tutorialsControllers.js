f29a865083984b6a8e9a70760cffd3649020c8eb
Jaimin Solanki <78346677+jaimin45@users.noreply.github.com>
2021-12-29 17:32:58 +0530

    Validations/joi (#22)
    * doc:APis
    
    * doc:APis
    
    * Initial commit
    
    * feat: Joi validations
    
    * feat: Joi validations
    
    * feat: Joi validations
    
    * docs: add line end of the code
    
    * docs: Controllers file
    
    * docs:controller and swagger files
    
    * fix: Update api
    
    * docs: small change in Controllers

--- controllers/tutorialsControllers.js
+++ controllers/tutorialsControllers.js
@@ -1,5 +1,6 @@
 const { CastError } = require("mongoose");
 const Tutorial = require("../models/tutorials");
+const { postTutorialSchema } = require("../validations/tutorials.validations");
 
 // get All Tutorials
 const getTutorials = async (req, res) => {
@@ -9,7 +10,7 @@ const getTutorials = async (req, res) => {
     });
     res.status(200).send(tutorials);
   } catch (error) {
-    res.status(500).send({ message: "Tutorial not found" });
+    res.status(500).send({ message: "Internal Server Error" });
   }
 };
 
@@ -21,41 +22,47 @@ const createTutorial = async (req, res) => {
     published: req.body.published,
   });
   try {
+    const { error } = postTutorialSchema.validate(req.body);
+    if (error instanceof CastError) {
+      return res.status(400).send({ message: error.message });
+    }
+    await postTutorialSchema.validateAsync(req.body);
     await tutorial.save();
     res.status(201).send();
   } catch (error) {
-    res.status(500).send({ message: "Internal Server Error" });
+    if (error) {
+      res.status(400).send({ message: "Invalid Tutorial Details" });
+    } else {
+      res.status(500).send({ message: "Internal Server Error" });
+    }
   }
+  return null;
 };
 
 // Update a Tutorial by ID
 const updateTutorial = async (req, res) => {
   try {
-    const { title, description, published } = req.body;
-    const update = {};
-    if (typeof title !== "undefined") update.title = title;
-    if (typeof description !== "undefined") update.description = description;
-    if (typeof published !== "undefined") update.published = published;
+    const { error, value } = postTutorialSchema.validate(req.body);
+    if (error) {
+      return res.status(400).send({ message: error.message });
+    }
     const tutorial = await Tutorial.findByIdAndUpdate(
       { _id: req.params.id },
-      update,
-      {
-        new: true,
-      }
+      value
     );
-
     if (tutorial === null) {
-      res.status(400).send({ message: "Invalid Tutorial Id" });
+      res.status(404).send({ message: "Tutorial not found" });
     } else {
       res.status(204).send();
     }
   } catch (error) {
     if (error instanceof CastError) {
-      res.status(400).send({ message: "Invalid Tutorial Id" });
+      res.status(404).send({ message: "Tutorial not found" });
     } else {
       res.status(500).send({ message: "Internal Server Error" });
     }
   }
+  return null;
 };
 
 // Delete a Tutorial by ID
